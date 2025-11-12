bdaa35dbdff572a61cf6fa7df95250f9d4ce076c
chandra-prakash-reddy <reddyp148@gmail.com>
2025-11-12 17:59:27 +0530

    test

--- wait_for.sh
+++ wait_for.sh
@@ -13,6 +13,7 @@ DEBUG="${DEBUG:-0}"
 TREAT_ERRORS_AS_READY="${TREAT_ERRORS_AS_READY:-0}"
 MAX_RETRIES="${MAX_RETRIES:-0}"
 
+
 usage() {
 cat <<EOF
 This script waits until a job, pod or service enter ready state.
